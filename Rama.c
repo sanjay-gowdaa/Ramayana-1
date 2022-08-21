5a47f99a8d11d402b20b50ae6f24f12029a81592
sanjay-gowdaa <sanjaysrinivasgowda@gmail.com>
2022-08-21 22:37:16 +0530

    sanjay origin changes

---
+++ Rama.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    for(i=0;i<n;i++){
+        i++;
+        printf("this is sanjay");
+    }
+}
---
+++ laki.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+int main(){
+    printf("how about a cofee");
+}
